2c6630c1c66599382d8dc6dd2ad251f7b9102d0c
James Palmer <james.palmer@itv.com>
2024-09-09 14:26:15 +0100

    add clever hello

--- fundamentals/hello.go
+++ fundamentals/hello.go
@@ -6,6 +6,15 @@ func Hello(name string) string {
 	return "Hello, " + name
 }
 
+const englishHelloPrefix = "Hello, "
+
+func CleverHello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
 func main() {
 	fmt.Println(Hello("James"))
 }
--- fundamentals/hello_test.go
+++ fundamentals/hello_test.go
@@ -26,3 +26,25 @@ To use the *testing.T type, you need to import "testing", like we did with fmt i
 
 For now, it's enough to know that your t of type *testing.T is your "hook" into the testing framework so you can do things like t.Fail() when you want to fail.
 */
+
+func TestCleverHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := CleverHello("James")
+		want := "Hello, James"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := CleverHello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
